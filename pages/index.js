41ec2040db49f1e2e9bf30f9b22c57f755da0585
Silvio Meyer <silvioenri@outlook.com>
2025-05-03 12:28:47 -0300

    Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is the home page.</p>
+    </div>
+  );    
+}
+export default Home;
+// This is a simple Next.js page component that renders a welcome message and a description.  
