264018f36814d2634fee263f15ea8778bc000fbc
Silvio Meyer <silvioenri@outlook.com>
2025-05-07 19:50:58 -0300

    Alterada a pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 function Home() {
   return (
-    <div style={{ backgroundColor: '#1a1a1a', minHeight: '100vh' }}>
-      <h1 style={{ color: 'blue' }}>Bem vindo a Página inicial!!</h1>
-      <p style={{ color: 'blue' }}>Certamente esta é a página inicial!</p>
+    <div
+      style={{
+        backgroundColor: '#1a1a1a',
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1 style={{ color: 'blue' }}>Olá senhora!</h1>
+      <p style={{ color: 'blue' }}>Gostaria de jogar um Areninho com migs?!</p>
+
+      <button
+        style={{
+          backgroundColor: 'blue',
+          color: 'white',
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+        }}
+        onClick={() => alert('Então entra no TeamSpeak 😎')}
+      >
+        Jogar
+      </button>
     </div>
-  );    
+  );
 }
 export default Home;
