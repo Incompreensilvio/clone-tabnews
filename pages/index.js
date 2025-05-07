5e4d376c2cc472d767f3a21e7faeddee638f0118
Silvio Meyer <silvioenri@outlook.com>
2025-05-07 19:34:14 -0300

    Alterado a cor do fundo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div>
+    <div style={{ backgroundColor: '#1a1a1a', minHeight: '100vh' }}>
       <h1 style={{ color: 'blue' }}>Bem vindo a Página inicial!!</h1>
       <p style={{ color: 'blue' }}>Certamente esta é a página inicial!</p>
     </div>
