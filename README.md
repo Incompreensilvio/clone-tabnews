a0012b515202454b608816d578d948b854511f2a
Silvio Meyer <silvioenri@outlook.com>
2025-07-15 20:04:17 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar programação baseado no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,27 @@ function Home() {
   return (
     <div
       style={{
-        backgroundColor: '#1a1a1a',
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
+        backgroundColor: "#1a1a1a",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
-      <h1 style={{ color: 'blue' }}>Olá senhora!</h1>
-      <p style={{ color: 'blue' }}>Gostaria de jogar um Areninho com migs?!</p>
+      <h1 style={{ color: "blue" }}>Olá senhora!</h1>
+      <p style={{ color: "blue" }}>Gostaria de jogar um Areninho com migs?!</p>
 
       <button
         style={{
-          backgroundColor: 'blue',
-          color: 'white',
-          padding: '10px 20px',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer',
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
         }}
-        onClick={() => alert('Então entra no TeamSpeak 😎')}
+        onClick={() => alert("Então entra no TeamSpeak 😎")}
       >
         Jogar
       </button>
